075ce450bfaa50ea9dab45119d51f9876c6e0dc8
Samir Jabre <samirjabre8@gmail.com>
2025-05-28 14:05:24 +0300

    Fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "192.168.1.108",
-    port: 3000,
-  },
+  // server: {
+  //   host: "192.168.1.115",
+  //   port: 3000,
+  // },
 });
